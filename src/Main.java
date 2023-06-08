4901e333cf25ce362f8ec2805f51053235b2770a
Evgen <Ni.Evgens@gmail.com>
2023-06-08 21:30:20 +0300

    First commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Programmer programmer = new Programmer("Evgen", "Kakaha");
+        System.out.println(programmer.work());
+
+    }
+}
---
+++ src/Programmer.java
@@ -0,0 +1,39 @@
+//Задача 6
+//Напишите класс Programmer.
+//
+//Переменные:
+//
+//name (имя, строка);
+//company (имя компании, строка);
+//position (позиция, строка);
+//конструктор, принимающий name и company;
+//метод getPosition(), возвращающий строку — позицию программиста.
+//метод work(), поднимающий позицию программиста на уровень вверх (с самого начала (при инициализации)
+//программист — intern (стажер), потом junior (джуниор), далее middle, senior, lead).
+public class Programmer {
+    String name;
+    String company;
+    String position;
+    public Programmer (String name, String company) {
+        this.name = name;
+        this.company = company;
+        this.position = "intern";
+    }
+
+
+    public String getPosition () {
+        return position;
+    }
+    public String work () {
+        if (position == "intern") {
+            position = "junior";
+        }else if (position == "junior") {
+            position = "middle";
+        }else if (position == "middle") {
+            position = "senior";
+        }else if (position == "senior") {
+            position = "lead";
+        }
+        return position;
+    }
+}
